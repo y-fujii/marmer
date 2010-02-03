7f5309637e65f9dd58739fdd7f363fe17c7dc456
Yasuhiro Fujii <y-fujii@mimosa-pudica.net>
2010-02-03 14:13:12 +0900

    fix

--- marmer.cpp
+++ marmer.cpp
@@ -3,10 +3,9 @@
 
 #include <exception>
 #include <iostream>
-#include <cmath>
+#include <algorithm>
 #include <boost/gil/gil_all.hpp>
 #include <boost/gil/extension/io/png_io.hpp>
-#include <boost/gil/extension/io/jpeg_io.hpp>
 
 using namespace std;
 
@@ -25,7 +24,8 @@ void minimizeNorm( SrcView const& src, DstView& dst ) {
 				double dx = (src(x + 1, y)[c] - src(x - 1, y)[c]);
 				double dy = (src(x, y + 1)[c] - src(x, y - 1)[c]);
 
-				if( dx * dx + dy * dy == 0.0 ) {
+				if( dx * dx + dy * dy < numeric_limits<double>::min() ) {
+					// denormalized number
 					dst(x, y)[c] = src(x, y)[c];
 				}
 				else {
@@ -45,13 +45,14 @@ void minimizeNorm( SrcView const& src, DstView& dst ) {
 		}
 	}
 
+	// copy boundary
 	for( int x = 0; x < w; ++x ) {
 		dst(x, 0) = src(x, 0);
-		dst(x, h) = src(x, h);
+		dst(x, h - 1) = src(x, h - 1);
 	}
 	for( int y = 1; y < h - 1; ++y ) {
 		dst(0, y) = src(0, y);
-		dst(w, y) = src(w, y);
+		dst(w - 1, y) = src(w - 1, y);
 	}
 }
 
@@ -65,7 +66,7 @@ struct Clip {
 	}
 };
 
-int main( int argc, char** argv ) {
+int main( int argc, char const* const* argv ) {
 	using namespace boost::gil;
 
 	// parse args
@@ -78,40 +79,35 @@ int main( int argc, char** argv ) {
 			throw exception();
 		}
 	}
-	catch( exception ) {
+	catch( exception const& ) {
 		cout << "Usage: " << argv[0] << " count src dst\n";
 		return 1;
 	}
 
-	// read image
-	rgb32f_image_t img0;
 	try {
+		// read image
+		rgb32f_image_t img0;
 		png_read_and_convert_image( argv[2], img0 );
-	}
-	catch( exception ) {
-		try {
-			jpeg_read_and_convert_image( argv[2], img0 );
-		}
-		catch( exception& e ) {
-			cout << e.what() << "\n";
-			return 1;
-		}
-	}
-	rgb32f_image_t img1( img0.dimensions() );
+		rgb32f_image_t img1( img0.dimensions() );
 
-	// process image
-	for( int i = 0; i < count; ++i ) {
-		cout << "\riter step: " << i * 2;
-		cout.flush();
-		minimizeNorm( view( img0 ), view( img1 ) );
-		minimizeNorm( view( img1 ), view( img0 ) );
-	}
-	cout << endl;
+		// process image
+		for( int i = 0; i < count; ++i ) {
+			cout << "\riter step: " << i;
+			cout.flush();
+			minimizeNorm( view( img0 ), view( img1 ) );
+			minimizeNorm( view( img1 ), view( img0 ) );
+		}
+		cout << endl;
 
-	// write image
-	rgb8_image_t dst( img0.dimensions() );
-	transform_pixels( view( img0 ), view( dst ), Clip() );
-	png_write_view( argv[3], view( dst ) );
+		// write image
+		rgb8_image_t dst( img0.dimensions() );
+		transform_pixels( view( img0 ), view( dst ), Clip() );
+		png_write_view( argv[3], view( dst ) );
+	}
+	catch( exception const& e ) {
+		cout << e.what() << endl;
+		return 1;
+	}
 
 	return 0;
 }
